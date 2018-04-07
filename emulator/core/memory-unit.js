666f4c02cf9f4a3fec8c7568eaab9eddfa59e0f1
Quentin 'Poe' Caffero <qcaffero@gmail.com>
2018-04-06 21:04:02 -0500

    - Updated: function stubs in memory unit

---
+++ emulator/core/memory-unit.js
@@ -0,0 +1,24 @@
+var MemoryUnit = function(){
+
+  this.init = function(){
+      //TODO;
+  };
+
+  this.readByte = function(addr){
+    //TODO
+  };
+
+  this.readWord = function(addr){
+    //TODO
+  };
+
+  this.writeByte = function(addr, val){
+    //TODO
+  };
+
+  this.writeWord = function(addr, val){
+    //TODO
+  };
+
+  this.init(...arguments);
+};
